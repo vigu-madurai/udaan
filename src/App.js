755b9f8c1fc3d8903d681c6d8e8f27b044e6fad5
Vigneshwaran Kannan <vkannan@quotient.com>
2021-09-26 02:27:58 +0530

    add pagination, offset

--- src/App.js
+++ src/App.js
@@ -10,14 +10,15 @@ function App() {
   const [accidentList, setAccidentList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [accident, setAccident] = useState("");
+  const [page, setPage] = useState(1);
+  const [limitList, setLimitList] = useState(5);
 
-
-  const fetchTrafficDetails = (limit = 5, offset = 0) => {
+  const fetchTrafficDetails = (offset = 1, limit = 5) => {
     axios.get(baseUrl, {
       params: {
         crash_date: `${selectedDate}T00:00:00.000`,
         $limit: limit,
-        $offset: offset
+        $offset: offset < 0 ? 0 : offset - 1
       }
 
     })
@@ -38,24 +39,54 @@ function App() {
   }
 
   const renderAlist = () => {
-    console.log("renderAlist", accidentList)
     if (accidentList == "No response found!") {
       return "No response found! Please select a different date"
     }
-    return accidentList.map((accident) => {
+    return accidentList.map((accidentEl) => {
       return (
-        <div key={accident.collision_id} className="card-wrapper" onClick={() => { setAccident(accident) }}>
+        <div key={accidentEl.collision_id} className="card-wrapper" onClick={() => { setAccident(accidentEl) }}>
           <div className="car-names">
-            <div>Crashed Car Type 1: {accident.vehicle_type_code1}</div>
-            <div> Crashed Car Type 2: {accident.vehicle_type_code2}</div>
+            {accidentEl.vehicle_type_code1 ? <div>Crashed Car Type 1: {accidentEl.vehicle_type_code1}</div> : ""}
+            {accidentEl.vehicle_type_code2 ? <div>Crashed Car Type 2: {accidentEl.vehicle_type_code2}</div> : ""}
           </div>
           <div className="details">
-            <div>Date: {accident.crash_date}</div>
-            <div>Time: {accident.crash_time}</div>
+            <div>Date: {accidentEl.crash_date}</div>
+            <div>Time: {accidentEl.crash_time}</div>
           </div>
         </div >
       )
     })
+  }
+
+  const setPagination = (p) => {
+    setPage(p);
+    fetchTrafficDetails(p, limitList);
+    setAccident("");
+  }
+
+  const setLimit = (l) => {
+    setLimitList(l);
+    fetchTrafficDetails(page, l);
+    setAccident("");
+  }
+
+  const renderPagination = () => {
+    return (
+      <>
+        <div className="limit-wrapper">
+          Display
+          <input type="number" minLength={5} value={limitList} onChange={(e) => { Number(e.target.value) <= 5 ? setLimit(5) : setLimit(Number(e.target.value)) }} />
+          Results
+        </div>
+        <div className="pagination-wrapper">
+          <button onClick={() => { setPagination(1) }}>First</button>
+          <button onClick={() => { setPagination(page > 1 ? page - 1 : 1) }}>Previous</button>
+          <input type="number" minLength={1} value={page} onChange={(e) => { setPagination(Number(e.target.value) <= 0 ? 1 : Number(e.target.value)) }} />
+          <button onClick={() => { setPagination(page + 1) }}>Next</button>
+          <button onClick={() => { setPagination(page + 10) }}>Next 10</button>
+        </div>
+      </>
+    )
   }
   useEffect(() => {
     if (accidentList.length) {
@@ -63,10 +94,14 @@ function App() {
     }
   }, [accidentList]);
 
+  const clearRecords = () => {
+    fetchTrafficDetails();
+    setAccident("");
+  }
+
   useEffect(() => {
     if (selectedDate) {
-      fetchTrafficDetails();
-      setAccident("");
+      clearRecords();
     }
   }, [selectedDate]);
 
@@ -75,15 +110,21 @@ function App() {
     setIsLoading(true);
     fetchTrafficDetails();
   }
-  console.log(accident)
+
   return (
     <div className="App">
       <div className="filter-wrapper">
         <ListAccident handleSelectedDate={handleSelectedDate} />
         {!isLoading ?
-          (<div className="crash-list-wrapper">
-            {renderAlist()}
-          </div>) : "Loading"
+          (
+            <>
+              <div className="crash-list-wrapper">
+                {renderAlist()}
+              </div>
+              {accidentList == "No response found!" ? "" : accidentList.length ?
+                renderPagination() : ""}
+            </>
+          ) : "Loading"
         }
       </div>
       {accident ? <ViewDetails data={accident} /> : ""}
