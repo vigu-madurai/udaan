3cdc66b994b1d72255c258bddc5239ebef6d9cbe
Vigneshwaran Kannan <vkannan@quotient.com>
2021-09-26 00:37:18 +0530

    ux cleanups

--- src/App.js
+++ src/App.js
@@ -11,18 +11,21 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [accident, setAccident] = useState("");
 
-  const fetchTrafficDetails = () => {
+
+  const fetchTrafficDetails = (limit = 5, offset = 0) => {
     axios.get(baseUrl, {
       params: {
-        crash_date: `${selectedDate}T00:00:00.000`
+        crash_date: `${selectedDate}T00:00:00.000`,
+        $limit: limit,
+        $offset: offset
       }
 
     })
       .then(function (response) {
-        if (response.status === 200) {
+        if (response.status === 200 && response.data && response.data.length) {
           setAccidentList(response.data);
         } else {
-          setAccidentList([])
+          setAccidentList("No response found!")
         }
       })
       .catch(function (error) {
@@ -36,14 +39,21 @@ function App() {
 
   const renderAlist = () => {
     console.log("renderAlist", accidentList)
+    if (accidentList == "No response found!") {
+      return "No response found! Please select a different date"
+    }
     return accidentList.map((accident) => {
       return (
         <div key={accident.collision_id} className="card-wrapper" onClick={() => { setAccident(accident) }}>
-          <div className="car-names">Crashed Cars Type: {accident.vehicle_type_code1} & {accident.vehicle_type_code2}
+          <div className="car-names">
+            <div>Crashed Car Type 1: {accident.vehicle_type_code1}</div>
+            <div> Crashed Car Type 2: {accident.vehicle_type_code2}</div>
           </div>
-          <div className="details"><div>{accident.crash_date}
-          </div> {accident.crash_time}</div>
-        </div>
+          <div className="details">
+            <div>Date: {accident.crash_date}</div>
+            <div>Time: {accident.crash_time}</div>
+          </div>
+        </div >
       )
     })
   }
@@ -55,7 +65,8 @@ function App() {
 
   useEffect(() => {
     if (selectedDate) {
-      fetchTrafficDetails()
+      fetchTrafficDetails();
+      setAccident("");
     }
   }, [selectedDate]);
 
