ac1b10ad624c75f69b8387dc0276733ad9fc8928
Vigneshwaran Kannan <vkannan@quotient.com>
2021-09-26 01:06:22 +0530

    view card

--- src/components/ViewDetails/index.js
+++ src/components/ViewDetails/index.js
@@ -1,11 +1,24 @@
 import React, { useState } from 'react';
+import './index.css';
 
 const ViewDetails = ({ data }) => {
-    
+
+    const keys = Object.keys(data);
+    const replaceUnderscore = (str) => {
+        return str.replace(/_/g, " ").toUpperCase()
+    }
+
     return (
         <>
             <div className="view-wrapper">
-                {JSON.stringify(data)}
+                <div className="view-inner-wrapper">
+                    {keys.map((el, index) => {
+                        return (<div className="row-wrapper" key={"view-" + index}>
+                            <div className="title">{replaceUnderscore(el)}:{" "}</div>
+                            <div>{data[el]}</div>
+                        </div>)
+                    })}
+                </div>
             </div>
         </>
     )
